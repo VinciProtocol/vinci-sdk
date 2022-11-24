6e2fe4f79677226c1db54bfda972cf15058dd251
rockgold0911 <rockgold0911@gmail.com>
2022-11-24 16:27:37 +0800

    feat(store): export vinciSDKReducers

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -12,3 +12,11 @@ export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppState, unkn
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
+
+import isApproved from './contract/erc20/isApproved'
+import reservesDataFromAllPools from './contract/uiPool/reservesDataFromAllPools'
+import userReservesDataFromAllPools from './contract/uiPool/userReservesDataFromAllPools'
+
+export const vinciSDKReducers = {
+  contract: { erc20: { isApproved }, uiPool: { reservesDataFromAllPools, userReservesDataFromAllPools } },
+}
